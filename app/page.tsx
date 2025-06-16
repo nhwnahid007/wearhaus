f82edd8fd1aaea705873d54abdb2c30ced661756
Md. Nahidul Islalm <nhwnahid@gmail.com>
2025-06-16 23:52:09 +0600

    added clerck auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Container from '../components/Container';
+import HomeBanner from '../components/HomeBanner';
 
 export default function Home() {
   return (
     <div className="">
-      <Container>
-        <h1>WearHaus ecommerce website</h1>
+      <Container className='py-10'>
+        
+        <HomeBanner/>
       </Container>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,14 @@ import Container from './Container';
 import MobileMenu from './MobileMenu';
 import SearchBar from './SearchBar';
 import CartIcon from './CartIcon';
+import { currentUser } from '@clerk/nextjs/server';
+import { ClerkLoaded, SignedIn, SignInButton, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import { ListOrdered } from 'lucide-react';
 
-const Header = () => {
+const Header = async () => {
+  const user = await currentUser();
+
   return (
     <header className="bg-white border-b py-5">
       {/* left bar */}
@@ -22,11 +28,24 @@ const Header = () => {
         <div className="w-auto flex items-center justify-end gap-5">
           <SearchBar />
           <CartIcon />
-          <div className="">
-            <button className="text-sm font-semibold hover:text-darkColor hoverEffect ">
-              Login
-            </button>
-          </div>
+          <ClerkLoaded>
+            <SignedIn>
+              <Link href={'/orders'} className="group relative">
+                <ListOrdered className="w-5 h-5 group-hover:text-darkColor hoverEffect" />
+                <span className="absolute -top-1 -right-1 bg-darkColor text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
+                  0
+                </span>
+              </Link>
+              <UserButton/>
+            </SignedIn>
+            {!user && (
+              <SignInButton mode="modal">
+                <button className="text-sm font-semibold hover:text-darkColor hoverEffect ">
+                  Login
+                </button>
+              </SignInButton>
+            )}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>
