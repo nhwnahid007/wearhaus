671590821cd10d6252e8ae1ea37f7d73c5af47b5
Md. Nahidul Islalm <nhwnahid@gmail.com>
2025-06-17 02:44:43 +0600

    added new font setup and created required user functionality

---
+++ app/orders/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { requiredUser } from '../../hooks/requiredUser';
+
+const OrdersPage = async () => {
+    await requiredUser();
+    return (
+        <div className=''>
+            Orders
+        </div>
+    );
+};
+
+export default OrdersPage;
